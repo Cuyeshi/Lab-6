4ecf37fc893f99733ab3264aa04db0ff0287337d
User <User@WIN-J9S0JJK5DN4>
2023-12-01 00:40:55 +0300

    Разработан класс Records для образования записи на приём к врачу.

--- LibraryForRecords/Records.cs
+++ LibraryForRecords/Records.cs
@@ -2,6 +2,24 @@
 {
     public class Records
     {
+        public string Initials;
 
+        public string Time;
+
+        public string Speciality;
+
+        public Records()
+        {
+            this.Initials = "";
+            this.Time = "00.00.0000";
+            this.Speciality = "";
+        }
+
+        public Records(string initials, string time, string speciality)
+        {
+            Initials = initials;
+            Time = time;
+            Speciality = speciality;
+        }
     }
 }
